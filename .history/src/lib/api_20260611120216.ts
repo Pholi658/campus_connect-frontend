2a0d536177920e47a89a635a30da33048d24a27f
Pholi658 <totipholiso@gmail.com>
2026-06-11 12:30:49 +0200

    fix offer bug

--- .history/src/lib/api_20260611120216.ts
+++ .history/src/lib/api_20260611120216.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Using relative paths to talk to our local Express proxy
 // This avoids CORS issues because the browser talks to the same origin
 const api = axios.create({
-  baseURL: 'https://campus-connect-backend-g7ul.onrender.com',
+  // baseURL: 'https://campus-connect-backend-g7ul.onrender.com',
   // baseURL: 'http://127.0.0.1:8000',
   headers: {
     'Content-Type': 'application/json',
